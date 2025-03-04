c1e275b30222a990066e6cd79233baa21a00270d
NerdsCoach <coachpeggy@nerdsonamission.org>
2025-03-03 18:59:12 -0600

    Auto Limit changed to 15 needs testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/GoBilda/DriveToPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/GoBilda/DriveToPoint.java
@@ -46,7 +46,7 @@ public class DriveToPoint
         IN_BOUNDS
     }
 
-    private static double xyTolerance = 12;
+    private static double xyTolerance = 15;
     private static double yawTolerance = 0.0349066;
 
     private static double pGain = 0.008;
